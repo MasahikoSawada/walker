1f1aeb8935e6bfc6c95b427075e7b9e3440e287c
Masahiko Sawada <sawada.mshk@gmail.com>
2017-12-20 19:35:45 +0900

    Add basic infrastructure to generate heat map.
    Each relation has a heat map, which is actually MAP_SIZE size array of
    char. Each MAP_SIZE_RANGE blocks belong to the same range. The current
    code report the status of a heatmap whenever increment its temprature.
    
    The most challenge I have is to map relfilenode to OID. It seems the
    similar component to snapshot builder will be required.

--- heatmap.c
+++ heatmap.c
@@ -21,6 +21,7 @@
 #include "access/xlogutils.h"
 #include "access/transam.h"
 #include "access/heapam_xlog.h"
+#include "lib/dshash.h"
 #include "miscadmin.h"
 #include "pgstat.h"
 #include "postmaster/bgworker.h"
@@ -33,6 +34,20 @@
 
 PG_MODULE_MAGIC;
 
+/* Heatmap array size */
+#define MAP_SIZE 1024
+
+/* Each MAP_RANGE_SIZE blocks belong to same index */
+#define MAP_RANGE_SIZE 32
+
+#define TargetBlknoToIndex(blkno) (blkno / MAP_RANGE_SIZE)
+
+typedef struct Heatmap
+{
+	RelFileNode	node;
+	char		hmap[MAP_SIZE];
+} Heatmap;
+
 /* Plugin handler function */
 extern void _PG_walw_plugin_init(WalwCallbacks *cb);
 
@@ -40,6 +55,10 @@ extern void _PG_walw_plugin_init(WalwCallbacks *cb);
 static void heatmap_startup(void);
 static void heatmap_heap(XLogReaderState *record);
 static void heatmap_heap2(XLogReaderState *record);
+
+static void HeatmapRecordChange(RelFileNode node, BlockNumber blkno);
+
+static HTAB *HeatmapHash;
 
 /* Handler function */
 void
@@ -56,7 +75,15 @@ _PG_walw_plugin_init(WalwCallbacks *cb)
 static void
 heatmap_startup(void)
 {
-	/* Initialize something */
+	HASHCTL	info;
+
+	info.keysize = sizeof(RelFileNode);
+	info.entrysize = sizeof(Heatmap);
+
+	HeatmapHash = hash_create("Heapmap hash",
+							  1024,
+							  &info,
+							  HASH_ELEM | HASH_BLOBS);
 }
 
 /*
@@ -65,28 +92,27 @@ heatmap_startup(void)
 static void
 heatmap_heap(XLogReaderState *record)
 {
-	uint8			info = XLogRecGetInfo(record) & ~XLR_INFO_MASK;
+	uint8		info = XLogRecGetInfo(record) & ~XLR_INFO_MASK;
+	RelFileNode node;
+	BlockNumber blkno;
 
 	switch (info)
 	{
 		case XLOG_HEAP_INSERT:
+		{
+			xl_heap_insert *xlrec;
+
+			xlrec = (xl_heap_insert *) XLogRecGetData(record);
+			XLogRecGetBlockTag(record, 0, &node, NULL, &blkno);
+			HeatmapRecordChange(node, blkno);
+		}
+		break;
 		case XLOG_HEAP_HOT_UPDATE:
 		case XLOG_HEAP_UPDATE:
 		case XLOG_HEAP_DELETE:
 		case XLOG_HEAP_INPLACE:
 		case XLOG_HEAP_CONFIRM:
 		case XLOG_HEAP_LOCK:
-		{
-			xl_heap_insert *xlrec;
-			RelFileNode node;
-			BlockNumber	blkno;
-
-			xlrec = (xl_heap_insert *) XLogRecGetData(record);
-			XLogRecGetBlockTag(record, 0, &node, NULL, &blkno);
-			ereport(LOG, (errmsg("heap: (%d, %d, %d) %d",
-								 node.spcNode, node.dbNode, node.relNode,
-								 blkno)));
-		}
 			break;
 		case XLOG_HEAP_INIT_PAGE:
 			break;
@@ -102,4 +128,43 @@ heatmap_heap(XLogReaderState *record)
 static void
 heatmap_heap2(XLogReaderState *record)
 {
+}
+
+static void
+HeatmapRecordChange(RelFileNode node, BlockNumber blkno)
+{
+	int index;
+	Heatmap *hm;
+	bool	found;
+
+	hm = (Heatmap *) hash_search(HeatmapHash,
+								 (void *) &node,
+								 HASH_ENTER, &found);
+
+	/* Initialize */
+	if (!found)
+	{
+		hm->node.spcNode = node.spcNode;
+		hm->node.dbNode = node.dbNode;
+		hm->node.relNode = node.relNode;
+		memset(hm->hmap, 0, MAP_SIZE);
+	}
+
+	index = TargetBlknoToIndex(blkno);
+
+	/* XXX : Make sure we don't overflow */
+	Assert(index < MAP_SIZE);
+
+	/* Increment temprature of the range */
+	hm->hmap[index]++;
+
+	{
+		int i;
+		for (i = 0; i < MAP_SIZE; i++)
+		{
+			if (hm->hmap[i] > 0)
+				ereport(LOG, (errmsg("node = %d, [%d] = %d",
+									 node.relNode, i, hm->hmap[i])));
+		}
+	}
 }
