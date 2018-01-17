fc3cdaa09dac0f96305a47fa2066c7ed03aa6c78
Masahiko Sawada <sawada.mshk@gmail.com>
2018-01-17 12:27:50 +0900

    Add sample png files.

--- garbagemap/garbagemap--1.0.sql
+++ garbagemap/garbagemap--1.0.sql
@@ -21,7 +21,7 @@ CREATE OR REPLACE FUNCTION gs(
        OUT n_tuples INT,
        OUT n_dead_tuples INT,
        OUT n_all_visible INT,
-       OUT dead_tuple_ratio NUMERIC(10,2))
+       OUT dead_tuple_ratio NUMERIC(15,4))
 RETURNS SETOF RECORD
 AS $$
 SELECT
@@ -30,7 +30,7 @@ SELECT
 	n_tuples,
 	n_dead_tuples,
 	n_all_visible,
-	(n_dead_tuples::numeric(10,2) / n_tuples)::numeric(10,2) as dead_tuple_ratio
+	(n_dead_tuples::numeric(15,4) / n_tuples)::numeric(15,4) as dead_tuple_ratio
 FROM garbage_summary(rel);
 $$
 LANGUAGE sql;
@@ -39,14 +39,14 @@ LANGUAGE sql;
 CREATE OR REPLACE FUNCTION gs_rank(
        rel regclass,
        OUT rownum INT,
-       OUT percent_blocks NUMERIC(10,2),
+       OUT percent_blocks NUMERIC(15,4),
        OUT rangeno INT,
        OUT freespace INT,
        OUT n_tuples INT,
        OUT n_dead_tuples INT,
        OUT n_all_visible INT,
-       OUT dead_tuple_ratio NUMERIC(10,2),
-       OUT percent NUMERIC(10,2))
+       OUT dead_tuple_ratio NUMERIC(15,4),
+       OUT percent NUMERIC(15,4))
 RETURNS SETOF RECORD
 AS $$
 SELECT
@@ -55,9 +55,9 @@ SELECT
        FROM
        (
               SELECT
-              (percent_rank() over(order by n_dead_tuples desc) * 100)::numeric(10,2) as percent_blocks,
+              (percent_rank() over(order by n_dead_tuples desc) * 100)::numeric(15,4) as percent_blocks,
               *,
-              round((sum(n_dead_tuples) over (order by n_dead_tuples desc range between unbounded preceding and current row))::numeric(10,2) / sum(n_dead_tuples) over () * 100, 2) as ratio
+              round((sum(n_dead_tuples) over (order by n_dead_tuples desc range between unbounded preceding and current row))::numeric(15,4) / nullif(sum(n_dead_tuples) over (), 0)::int * 100, 2) as ratio
               FROM gs(rel)
               ORDER BY n_dead_tuples DESC, rangeno ASC
       ) as foo
