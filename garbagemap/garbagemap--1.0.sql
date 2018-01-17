a5734270388c60223872de265a1e7f146db1e965
Masahiko Sawada <sawada.mshk@gmail.com>
2018-01-17 10:32:44 +0900

    Make gs_rank() output row number.

--- garbagemap/garbagemap--1.0.sql
+++ garbagemap/garbagemap--1.0.sql
@@ -38,18 +38,28 @@ LANGUAGE sql;
 -- Ranking of ranges having dead tuples */
 CREATE OR REPLACE FUNCTION gs_rank(
        rel regclass,
+       OUT rownum INT,
+       OUT percent_blocks NUMERIC(10,2),
        OUT rangeno INT,
        OUT freespace INT,
        OUT n_tuples INT,
        OUT n_dead_tuples INT,
        OUT n_all_visible INT,
        OUT dead_tuple_ratio NUMERIC(10,2),
-       OUT precent NUMERIC(10,2))
+       OUT percent NUMERIC(10,2))
 RETURNS SETOF RECORD
 AS $$
-SELECT *,
-       round((sum(n_dead_tuples) over (order by n_dead_tuples desc range between unbounded preceding and current row))::numeric(10,2) / sum(n_dead_tuples) over () * 100, 2)
-       FROM gs(rel)
-       ORDER BY n_dead_tuples DESC, rangeno ASC;
+SELECT
+       (row_number() over())::INT as rownum,
+       foo.*
+       FROM
+       (
+              SELECT
+              (percent_rank() over(order by n_dead_tuples desc) * 100)::numeric(10,2) as percent_blocks,
+              *,
+              round((sum(n_dead_tuples) over (order by n_dead_tuples desc range between unbounded preceding and current row))::numeric(10,2) / sum(n_dead_tuples) over () * 100, 2) as ratio
+              FROM gs(rel)
+              ORDER BY n_dead_tuples DESC, rangeno ASC
+      ) as foo
 $$
 LANGUAGE sql;
